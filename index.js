257b5d786698cae8f468fbc5308c2fa992cd4ec5
achou11 <chouandrewo@gmail.com>
2019-04-28 13:01:40 -0400

    Add some state for future features

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 const captionForm = document.querySelector('.caption-form')
 const captionInput = captionForm.querySelector('[name=caption-input]')
 const newCartoonButton = document.querySelector('.get-cartoon')
@@ -7,6 +7,11 @@ const newCaption = document.querySelector('.new-caption')
 
 const ENDPOINT = 'https://www.newyorker.com/cartoons/random/randomAPI'
 
+const state = {
+  id: null,
+  src: '',
+  caption: ''
+}
 
 function createSourceLink () {
   const sourceUrl = document.getElementById('source-url')
@@ -19,30 +24,52 @@ function createSourceLink () {
 }
 
 function getCartoon () {
-  fetch(ENDPOINT)
+  window
+    .fetch(ENDPOINT)
     .then(res => res.json())
-    .then((data) => {
+    .then(data => {
       const cartoon = data[0]
-      
+
       reset()
-      
-      cartoonImage.setAttribute('src', cartoon.src)
+
+      update(cartoon)
     })
-    .catch((err) => console.error(err))
+    .catch(err => console.error(err))
+}
+
+function update (cartoon) {
+  state.id = cartoon.id
+  state.src = cartoon.src
+  cartoonImage.setAttribute('src', state.src)
 }
 
 function processSubmit (e) {
   e.preventDefault()
 
-  newCaption.textContent = captionInput.value.length > 0 ? `"${captionInput.value}"` : ''
+  newCaption.textContent =
+    captionInput.value.length > 0 ? `"${captionInput.value}"` : ''
+
+  window.localStorage.setItem(
+    state.id,
+    JSON.stringify({
+      src: state.src,
+      caption: captionInput.value.length > 0 ? `"${captionInput.value}"` : ''
+    })
+  )
 }
 
-function reset () {  
+function reset () {
   captionForm.reset()
   newCaption.textContent = ''
+  state.id = null
+  state.src = null
 }
 
+function clearLocalStorage () {
+  window.localStorage.clear()
+}
 document.addEventListener('DOMContentLoaded', getCartoon)
 document.addEventListener('DOMContentLoaded', createSourceLink)
+document.addEventListener('DOMContentLoaded', clearLocalStorage)
 newCartoonButton.addEventListener('click', getCartoon)
 captionForm.addEventListener('submit', processSubmit)
